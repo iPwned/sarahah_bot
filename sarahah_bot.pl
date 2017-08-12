f97baaf3d69461159dbb4bc60557c89feb06fd5f
Christopher Betancourt <christopher.betancourt@gmail.com>
2017-08-11 20:19:52 -0400

    Separate out header data

--- sarahah_bot.pl
+++ sarahah_bot.pl
@@ -6,21 +6,21 @@ use WWW::Curl::Easy;
 
 {
 	my $curl = WWW::Curl::Easy->new;
-	my $respBody;
 	my $user = shift;
-	my $retCode;
+	my ($respHeaders, $respBody, $respCode, $retCode);
 
-	$curl->setopt(CURLOPT_HEADER, 1);
 	$curl->setopt(CURLOPT_URL, "https://$user.sarahah.com");
 	$curl->setopt(CURLOPT_WRITEDATA, \$respBody);
+	$curl->setopt(CURLOPT_HEADERDATA, \$respHeaders);
 	$retCode = $curl->perform;
 
 	if($retCode == 0)
 	{
 		print "curl call succeeded\n";
 
-		my $respCode=$curl->getinfo(CURLINFO_HTTP_CODE);
+		$respCode=$curl->getinfo(CURLINFO_HTTP_CODE);
 		print "HTTP Response code: $respCode\n";
+		print "Headers:\n$respHeaders\n";
 		print "Response:\n$respBody\n";
 	}
 	else
